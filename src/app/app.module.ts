1c8fc0095d59b19fcb4f11229e7b7db31ccc4059
Josue Guardado <jguardadosv@gmail.com>
2022-08-16 16:45:40 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {SidebarModule} from 'primeng/sidebar';
+import {ButtonModule} from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SidebarModule,
+    ButtonModule,BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
